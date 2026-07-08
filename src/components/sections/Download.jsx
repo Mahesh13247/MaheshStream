f933263c5b9afb1ef78404e6003683bc6bf55716
Mahesh13247 <139057471+Mahesh13247@users.noreply.github.com>
2026-07-08 23:44:48 +0530

    update

--- src/components/sections/Download.jsx
+++ src/components/sections/Download.jsx
@@ -643,7 +643,7 @@ export default function Download() {
         </motion.div>
 
         {/* Direct Download Link for Desktop Users */}
-        <motion.div
+        {/* <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={isInView ? { opacity: 1, y: 0 } : {}}
           transition={{ duration: 0.5, delay: 0.6, ease: [0.16, 1, 0.3, 1] }}
@@ -681,7 +681,7 @@ export default function Download() {
           >
             {getApkAbsoluteUrl().replace(/^https?:\/\//, '')}
           </a>
-        </motion.div>
+        </motion.div> */}
       </div>
     </Section>
   )
